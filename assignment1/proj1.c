bf09dd5a34aca129d608cc936a3f6998308a3e42
NicholasFay-CIS <nfay@uoregon.edu>
2019-10-08 10:28:52 -0700

    adding project1

---
+++ assignment1/proj1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "command.h"
+#include <sys/syscall.h>
+#include <unistd.h>
+
+
+void listDir() {
+
+
+}
+
+int main(int argc, char *argv[]) {
+	setbuf(stdout, NULL);
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+
+	while(1) {
+		printf(">>> ");
+		getline(&line, &len, stdin);
+		if(strcmp(line, "exit\n") == 0) {
+			free(line);
+			exit(EXIT_SUCCESS);
+		}
+		if(strcmp(line, "\n") != 0) {
+
+
+		}
+			
+
+
+	}
+	return 1;
+} 

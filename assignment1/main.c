bb3d0352bf9f6d28f2e7e0e396f4062b2fa9ccbf
NicholasFay-CIS <nfay@uoregon.edu>
2019-10-16 16:42:54 -0700

    updating main

--- assignment1/main.c
+++ assignment1/main.c
@@ -17,13 +17,27 @@ int main(int argc, char *argv[]) {
 	const char dil[2] = " ";
 	FILE *fout;
 	FILE *fin;
-	int filemode = 0;
+	int filemode;
 
 	if(argc > 3) 
 	{
 		printf("Error: too many arguments given\n");
 		return 1;
 	} 
+	if(argc == 3) {
+		if(strcmp(argv[1], "-f") == 0) {
+			int fd;
+			fd = open(argv[2], O_RDONLY);
+			if(fd != -1) {
+				filemode = 1;
+			} else {
+				printf("Not able to open file. File descriptor is invalid");
+			}
+		} else {
+			printf("Unrecognized run command.\n");
+			return;
+		}
+	}
 	if(argc == 2)
 	{
 		if(strcmp(argv[1], "-command") != 0)
@@ -31,196 +45,259 @@ int main(int argc, char *argv[]) {
 			printf("Error: input argument failure\n");
 			return;
 		}
-	}														
-	while(exit != 1)
-	{	
-		int good = 0;
-		char *param;
-		char *param2;
-		int valid = 0;
-		printf(">>> ");
-		getline(&line, &len, stdin);
-		char *array[sizeof(line)];
-		int i = 0;
-		int count = 0;
-		token = strtok(line, dil);
-		while(token != NULL) {
-			array[i] = token;
-			if(array[i][strlen(array[i])-1] == '\n')
-			{
-				array[i][strlen(array[i])-1] = 0;
-			}
-			i++;
-			count++;
-			token = strtok(NULL, dil);
-		}
-		array[i] = "\0";
-		i = 0;
-		while(array[i] != "\0") {
-
-			valid = isCMD(array[i]);
-			if(strcmp(array[i], "exit") == 0 || strcmp(array[i], "exit\n") == 0) {
-				exit = 1;
-				break;
-			}
-			else if(strcmp(array[i], "\n") == 0)
-			{
-				break;
-			}
-			else if(strcmp(array[i], "ls") == 0)
-			{	
-				valid = isCMD(array[i+1]);
-				if(strcmp(array[i+1], ";") != 0 && strcmp(array[i+1], "\0") != 0) 
-				{
-					printf("Error!: Unsupported arguments for command: ls\n");	
-					break;
-				}
-				if(strcmp(array[i], "ls") == 0 && valid != 1) 
-				{
-					listDir();
-				} else {
-					printf("Error!: Unsupported arguments for command: ls\n");
-					break;
-				}
-			}
-			else if(strcmp(array[i], "pwd") == 0)
-			{
-				valid = isCMD(array[i+1]);
-				if(strcmp(array[i+1], ";") != 0 && strcmp(array[i+1], "\0") != 0)
-				{
-					printf("Error!: Unsupported arguments for command: pwd\n");
-					break;
-				}
-				if(strcmp(array[i], "pwd") == 0 && valid != 1)
-				{
-					showCurrentDir();
-				} else {
-					printf("Error!: Unsupported arguments for command: pwd\n");
-					break;
-				}
-			}
-			else if(strcmp(array[i], "cd") == 0)
-			{
-				valid = isCMD(array[i+1]);
-				if((strcmp(array[i+2], ";") == 0 && count == 3) || count == 1 )
-				{
-					printf("Error!: Unsupported arguments for command: cd\n");
-					break;
-				}
-				if((strcmp(array[i+2], "\0") == 0) || (count > 3 && strcmp(array[i+2], ";") == 0))
-				{	char *param = array[i+1];
-					changeDir(param);
-					good = 1;
-				} else {
-					printf("Error!: Unsupported arguments for command: cd\n");
-					break;
-				}
-			}
-			else if(strcmp(array[i], "cat") == 0)
-			{
-				if((strcmp(array[i+2], ";") == 0 && count == 3) || count == 1)
-				{
-					printf("Error!: Unsupported arguments for command: cat\n");
-					break;
-				}
-				if((strcmp(array[i+2], "\0") == 0) || (count > 3 && strcmp(array[i+2], ";") == 0))
-				{
-					char *param = array[i+1];
-					displayFile(param);
-					good = 1;
-				} else {
-					printf("Error!: Unsupported arguments for command: cat\n");
-					break;
-				}
-			}
-			
-			else if(strcmp(array[i], "rm") == 0)
-			{
-				if((strcmp(array[i+2], ";") == 0 && count == 3) || count == 1)
-				{
-					printf("Error!: Unsupported arguments for command: rm\n");
-					break;
-				}
-				if((strcmp(array[i+2], "\0") == 0) || (count > 3 && strcmp(array[i+2], ";") == 0))	
-				{	char *param = array[i+1];
-					deleteFile(param);
-					good = 1;
-				} else {
-					printf("Error!: Unsupported arguments for command: rm\n");
-					break;
-				}
-			}
-			
-			else if(strcmp(array[i], "mkdir") == 0)
-			{
-				if((strcmp(array[i+2], ";") == 0 && count == 3) || count == 1)
-				{
-					printf("Error!: Unsupported arguments for command: mkdir\n");
-					break;
-				}
-				if((strcmp(array[i+2], "\0") == 0) || (count > 3 && strcmp(array[i+2], ";") == 0))
-				{		
-					char *param = array[i+1];
-					makeDir(param);
-					good = 1;
-				} else {
-					printf("Error!: Unsupported arguments for command: mkdir\n");
-					break;
-				}
-			}		
-			else if(strcmp(array[i], "mv") == 0)
-			{
-				if(strcmp(array[i+1], "\0") == 0 || strcmp(array[i+2], "\0") == 0) 
-				{
-					printf("Error!: Unsupported arguments for command: mv\n");
-					break;
-				}
-				if((strcmp(array[i+3], "\0") == 0) || (count > 4 && strcmp(array[i+3], ";") == 0))
-				{		
-					char *param = array[i+1];
-					char *param2 = array[i+2];
-					moveFile(param, param2);
-					good = 1;
-				} else {
-					printf("Error!: Unsupported arguments for command: mv\n");
-					break;
-				}
-			}
-			
-			else if(strcmp(array[i], "cp") == 0)
-			{
-				if(strcmp(array[i+1], "\0") == 0 || strcmp(array[i+2], "\0") == 0) 
-				{
-					printf("Error!: Unsupported arguments for command: cp\n");
-					break;
-				}
-				if((strcmp(array[i+3], "\0") == 0) || (count > 4 && strcmp(array[i+3], ";") == 0))
-				{		
-					char *param = array[i+1];
-					char *param2 = array[i+2];
-					moveFile(param, param2);
-					good = 1;
-				} else {
-					printf("Error!: Unsupported arguments for command: cp\n");
-					break;
-				}
-			}
-			else if(strcmp(array[i], ";") == 0) {
-				good = 0;
-			}
-			else {
-				if(good != 1 && strcmp(array[i], ";") != 0) { 
-					printf("else: %s\n", array[i]);
-				}
-			}
-			i++;
-		}
+		filemode = 0;
+		STREAM = 1;
+	} 
+	if(argc == 1) {
+		printf("Error!:Invalid run commands.");
+		return;
 	}
-	if(filemode == 1) 
-	{
+	if(filemode == 0) 
+	{														
+		while(exit != 1)
+		{
+			int valid = 0;
+			printf(">>> ");
+			getline(&line, &len, stdin);
+			int i = 0;
+			int count = 0;
+			char *array[sizeof(line)];
+			token = strtok(line, dil);
+			while(token != NULL) {
+				array[i] = token;
+				if(array[i][strlen(array[i])-1] == '\n')
+				{
+					array[i][strlen(array[i])-1] = 0;
+				}
+				i++;
+				count++;
+				token = strtok(NULL, dil);
+			}
+			array[i] = "\0";
+			i = 0;
+			int cmd_p, param1_p, param2_p;
+			while(array[i] != NULL) {
+				char *param1;
+				char *param2;
+				if(i >= count) {
+					break;
+				}
+				if(strcmp(array[i], "exit") == 0) {
+					exit = 1;
+					break;
+				}
+				if(strcmp(array[i], "\n") == 0)
+				{
+					break;
+				}
+				int is_cc;
+				//check 1. get token cmd position:
+				char *command = array[i];
+				cmd_p = i;
+				valid = isCMD(command); //A either 1 or 0 for checking command
+				if(valid) {
+					i++;
+				}
+				if(strcmp(array[i], " \0") == 0 || strcmp(array[i], "\0") == 0) {
+					break;
+				}
+				if(!valid) {
+					printf("Error!: Unrecognized comnmand: %s.\n", command);
+					break;
+				}
+				//check 2. get token and param1 position
+				 //A either 1 or 0 checking command
+				if(count <= i || strcmp(array[i], "\0") == 0 || strcmp(array[i], ";") == 0) { //B either 1 or 0 for checking control code
+					if(strcmp(command, "ls") == 0) {
+						listDir();
+					}
+					if(strcmp(command, "pwd") == 0) {
+						showCurrentDir();
+					}
+					i++;
+					continue;
+				}
+				param1 = array[i];
+				valid = isCMD(param1);
+				if(valid) {
+					printf("Error!: Incorrect syntax. No control code found.\n");
+					break;
+				}
+				i++;
+				//check 2. get token and param1 position
+				 //A either 1 or 0 checking command
+				if(strcmp(array[i], "\0") == 0 || strcmp(array[i], ";") == 0) { //B either 1 or 0 for checking control code
+					if(strcmp(command, "cd") == 0) {
+						changeDir(array[i-1]);
+					}
+					if(strcmp(command, "rm") == 0) {
+						deleteFile(array[i-1]);
+					}
+					if(strcmp(command, "cat") == 0) {
+						displayFile(array[i-1]);
+					}
+					if(strcmp(command, "mkdir") == 0) {
+						makeDir(array[i-1]);
+					}
+					i++;
+					continue;
+				}
+				param2 = array[i];
+				valid = isCMD(param1);
+				if(valid) {
+					printf("Error!: Incorrect syntax. No control code found.\n");
+					break;
+				}
+				if(strcmp(command, "mv") == 0) {
+						moveFile(array[i-1],array[i]);
+				}
+				if(strcmp(command, "cp") == 0) {
+						copyFile(array[i-1], array[i]);
+				}
+				i++;
+				continue;
+ 			}	
+		}
+		free(line);
+		return 1;
+	}
+	if(filemode == 1) {
+		FILE *fin;
+		int fout;
+		char *line = NULL;
+		ssize_t nread;
+		size_t len = 0;
+		fin = fopen(argv[2], "r");
+		//fout = freopen("output.txt", "w+", stdout);
+		fout = open("output.txt", O_WRONLY | O_CREAT, S_IRWXU);
+		STREAM = fout;
+		nread = getline(&line, &len, fin);
+		do {
+			int valid = 0;
+			int i = 0;
+			int count = 0;
+			char *array[sizeof(line)];
+			token = strtok(line, dil);
+			while(token != NULL) {
+				array[i] = token;
+				if(array[i][strlen(array[i])-1] == '\n')
+				{
+					array[i][strlen(array[i])-1] = 0;
+				}
+				i++;
+				token = strtok(NULL, dil);
+			}
+			array[i] = "\0";
+			i = 0;
+			int cmd_p, param1_p, param2_p;
+			while(array[i] != NULL) {
+				char *param1;
+				char *param2;
+				if(strcmp(array[i], "exit") == 0) {
+					exit = 1;
+					break;
+				}
+				if(strcmp(array[i], "\n") == 0)
+				{
+					break;
+				}
+				int is_cc;
+				//check 1. get token cmd position:
+				char *command = array[i];
+				cmd_p = i;
+				valid = isCMD(command); //A either 1 or 0 for checking command
+				if(valid) {
+					i++;
+				}
+				if(strcmp(array[i], "\0") == 0) {
+					break;
+				}
+				if(!valid) {
+					printf("Error!: Unrecognized syntax. \n");
+					break;
+				}
+				//check 2. get token and param1 position
+				 //A either 1 or 0 checking command
+				if(array[i] == NULL) {
+					is_cc = 1;
+					if(strcmp(command, "ls") == 0) {
+						listDir();
+					}
+					if(strcmp(command, "pwd") == 0) {
+						showCurrentDir();
+					}
+					i++;
+					continue;
+				}
+				if(strcmp(array[i], "\0") == 0 || strcmp(array[i], ";") == 0) { //B either 1 or 0 for checking control code
+					if(strcmp(command, "ls") == 0) {
+						listDir();
+					}
+					if(strcmp(command, "pwd") == 0) {
+						showCurrentDir();
+					}
+					i++;
+					continue;
+				}
+				param1 = array[i];
+				valid = isCMD(param1);
+				if(valid) {
+					printf("Error!: Incorrect syntax. No control code found.\n");
+					break;
+				}
+				i++;
+				//check 2. get token and param1 position
+				 //A either 1 or 0 checking command
+				if(array[i] == NULL) {
+					if(strcmp(command, "cd") == 0) {
+						changeDir(array[i-1]);
+					}
+					if(strcmp(command, "rm") == 0) {
+						deleteFile(array[i-1]);
+					}
+					if(strcmp(command, "cat") == 0) {
+						displayFile(array[i-1]);
+					}
+					if(strcmp(command, "mkdir") == 0) {
+						makeDir(array[i-1]);
+					}
+					i++;
+					continue;
+				}
+				if(strcmp(array[i], "\0") == 0 || strcmp(array[i], ";") == 0) { //B either 1 or 0 for checking control code
+					if(strcmp(command, "cd") == 0) {
+						changeDir(array[i-1]);
+					}
+					if(strcmp(command, "rm") == 0) {
+						deleteFile(array[i-1]);
+					}
+					if(strcmp(command, "cat") == 0) {
+						displayFile(array[i-1]);
+					}
+					if(strcmp(command, "mkdir") == 0) {
+						makeDir(array[i-1]);
+					}
+					i++;
+					continue;
+				}
+				param2 = array[i];
+				valid = isCMD(param1);
+				if(valid) {
+					printf("Error!: Incorrect syntax. No control code found.\n");
+					break;
+				}
+				if(strcmp(command, "mv") == 0) {
+						moveFile(array[i-1],array[i]);
+				}
+				if(strcmp(command, "cp") == 0) {
+						copyFile(array[i-1], array[i]);
+				}
+				i++;
+				continue;
+ 			}
+		} while((nread = getline(&line, &len, fin) != -1));
+		free(line);
 		fclose(fin);
-		fclose(fout);
-	}
-	free(line);
-	return 1; 	 
+		close(fout);
+	} 	 
 }
